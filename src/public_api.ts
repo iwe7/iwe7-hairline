e9fa9768b4bb01b9aff635c9bfbb3c1bcc991c7e
iwe7 <admin@meepo.com.cn>
2018-06-08 16:31:16 +0800

    update

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,7 +1,2 @@
-/*
- * Public API Surface of iwe7-hairline
- */
 
-export * from './lib/iwe7-hairline.service';
-export * from './lib/iwe7-hairline.component';
 export * from './lib/iwe7-hairline.module';
